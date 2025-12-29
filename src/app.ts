c45a3c14ea245d2b5f57a7ed75ba49180f30f186
nuralom787 <md9577630@gmail.com>
2025-12-29 20:09:36 +0600

    add nodemailler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 import { PostRouter } from './modules/post/post.router';
+import { auth } from './lib/auth';
+import { toNodeHandler } from "better-auth/node";
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin: process.env.APP_URL || 'http://localhost:5000',
+    credentials: true,
+}));
+
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 
 app.use('/posts', PostRouter);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,64 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
-// If your Prisma file is located elsewhere, you can change the path
-// import { PrismaClient } from "@/generated/prisma/client";
+
+import nodemailer from "nodemailer";
+
+// const testAccount = await nodemailer.createTestAccount();
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
 
 export const auth = betterAuth({
+    trustedOrigins: [process.env.APP_URL || "http://localhost:5000"],
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true,
+    },
+    user: {
+        additionalFields: {
+            role: {
+                type: "string",
+                defaultValue: "user",
+                required: false,
+                input: false
+            },
+            phone: {
+                type: "string",
+                required: false,
+                input: true
+            },
+            status: {
+                type: "string",
+                defaultValue: "active",
+                required: false,
+            }
+        }
+    },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+
+            const verificationURL = `${process.env.APP_URL}/verify-email?token=${token}`;
+
+            const info = await transporter.sendMail({
+                from: '"Learn Prisma" <alamn7150@gmail.com>',
+                to: "md9577630@gmail.com",
+                subject: "Verify your email address",
+                text: `Click the link to verify your email: ${url}`,
+            });
+
+            console.log("Verification Email Sent!!");
+        },
+    }
 });
